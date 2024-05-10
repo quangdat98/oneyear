b79d21483a76743b7bcc7712ddeb700a23d7d5e0
Nguyen Quang Dat <datnq@dc1.gemvietnam.com>
2024-05-10 13:30:51 +0700

    88%

--- src/home.tsx
+++ src/home.tsx
@@ -2,19 +2,35 @@ import React from "react";
 import "./homeCss.css";
 import "./homeScript.js";
 import "./bootstrap.css";
-import "./awesome.css";
-import image2 from './images/image2.png'; 
-import image3 from './images/image3.png';
-import image4 from './images/image4.png';
+import image2 from "./images/image2.png";
+import image3 from "./images/image3.png";
 
 export const home = (props) => {
+  const imageFoot = [];
+  for( let i =1; i<= 120; i++) {
+    imageFoot.push(
+      <div key={i}
+      className="image"
+      style={{
+        "--i": i,
+        "--url": `url('https://picsum.photos/200?v=${i}')`,
+      }}
+    ></div>
+    )
+  }
+
   return (
     <div className="body">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
       <div className="animation-element bounce-up">
         <div className="paral paralsec jumbotron bounce-up" id="about">
           <div className="desc">
-            <span className="title">Cảm Ơn Tể đã đồng hành cùng Bún 1 năm qua</span>
+            <span className="title">
+              Cảm Ơn Tể đã đồng hành cùng Bún 1 năm qua
+            </span>
           </div>
         </div>
       </div>
@@ -24,14 +40,10 @@ export const home = (props) => {
           className="jumbotron container-fluid"
           style={{ backgroundColor: "#fff" }}
         >
-          <h1>About the Bride</h1>
+          <h1>Quan tâm</h1>
           <div className="row">
             <div className="col-md-6">
-              <img
-                src={image2}
-                alt="Bride"
-                className="thumb"
-              />
+              <img src={image2} alt="Bride" className="thumb" />
             </div>
             <div className="col-md-6">
               <p className="info">
@@ -51,7 +63,7 @@ export const home = (props) => {
 
       <div className="animation-element bounce-up">
         <div className="jumbotron container-fluid">
-          <h1>About the Groom</h1>
+          <h1>Giận Dỗi</h1>
           <div className="row">
             <div className="col-md-6">
               <p className="info">
@@ -66,11 +78,7 @@ export const home = (props) => {
               </p>
             </div>
             <div className="col-md-6">
-              <img
-                src={image3}
-                alt="Groom"
-                className="thumb"
-              />
+              <img src={image3} alt="Groom" className="thumb" />
             </div>
           </div>
         </div>
@@ -78,20 +86,38 @@ export const home = (props) => {
 
       <div className="animation-element bounce-up">
         <div className="paral paralsec1 jumbotron">
-          <div className="desc" style={{ backgroundColor: "rgba(1,1,1,.3)" }}>
-            <span className="title title2">Join us as we become one</span>
-            <hr />
-            <br />
-            <br />
-            <br />
-            <span style={{ color: "#fff" }}>
-              May 29, 2018
-              <br />
-              3:00 p.m.
-            </span>
-            <p>
-              <button className="button">RSVP</button>
-            </p>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
+            rel="stylesheet"
+          />
+          <div className="perspective">
+            <div className="content-div">
+              <h1>Abhishek</h1>
+            </div>
+            <div className="img-cylinder">
+                {imageFoot}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="animation-element bounce-up">
+        <div className="paral paralsec1 jumbotron">
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
+            rel="stylesheet"
+          />
+          <div className="perspective">
+            <div className="content-div">
+              <h1>Abhishek</h1>
+            </div>
+            <div className="img-cylinder">
+                {imageFoot}
+            </div>
           </div>
         </div>
       </div>
